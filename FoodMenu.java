fc9830bbdc811515032f95d6ad81c7247f68c9eb
maderalkirk <102456343+maderalkirk@users.noreply.github.com>
2025-11-09 12:54:48 +0800

    added FoodMenu.java

---
+++ FoodMenu.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class FoodMenu {
+    // array for menu
+    private ArrayList<Food> menu;
+
+    public FoodMenu(){
+        menu = new ArrayList<>();
+
+        // add items
+        menu.add(new MainDish("Chicken Katsu", 150, true));
+        menu.add(new Drinks("Red Iced Tea", 70, "Large"));
+    }
+
+    // display menu
+    public void displayMenu(){
+        System.out.println("==Food Menu==");
+
+        for(int i = 0; i < menu.size(); i++){
+            System.out.print("[" + (i+1) + "]" );
+            menu.get(i).displayInfo();
+        }
+    }
+
+    // return food from the array
+    public Food getFood(int index){
+        return menu.get(index);
+    }
+
+    // return the size of the array
+    public int getMenuSize(){
+        return menu.size();
+    }
+}
