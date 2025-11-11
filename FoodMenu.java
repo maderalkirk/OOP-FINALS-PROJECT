406eea0eb1692113562b1d746f86492bd8a4f46a
Vince Gio Acedillo <121595186+binsacedillo@users.noreply.github.com>
2025-11-11 14:56:10 +0800

    Refactor FoodMenu to use array instead of ArrayList

--- FoodMenu.java
+++ FoodMenu.java
@@ -1,35 +1,33 @@
 package com.mycompany.finalsmain;
 
-import java.util.ArrayList;
-
 public class FoodMenu {
 
-    private final ArrayList<Food> menu;
+    private final Food[] menu;
 
     public FoodMenu() {
-        menu = new ArrayList<>();
-        menu.add(new MainDish("Chicken Katsu", 150, true));
-        menu.add(new MainDish("Pork Katsu", 150, true));
-        menu.add(new MainDish("Chicken Katsu Curry", 179, true));
-        menu.add(new MainDish("Chicken Teriyaki Bowl", 130, true));
-        menu.add(new Drinks("Red Iced Tea", 70, "Large"));
-        menu.add(new Drinks("Bottled Water", 30, "16oz"));
-        menu.add(new Drinks("Soda in Can", 45, "12oz"));
+        menu = new Food[7];
+        menu[0] = new MainDish("Chicken Katsu", 150, true);
+        menu[1] = new MainDish("Pork Katsu", 150, true);
+        menu[2] = new MainDish("Chicken Katsu Curry", 179, true);
+        menu[3] = new MainDish("Chicken Teriyaki Bowl", 130, true);
+        menu[4] = new Drinks("Red Iced Tea", 70, "Large");
+        menu[5] = new Drinks("Bottled Water", 30, "16oz");
+        menu[6] = new Drinks("Soda in Can", 45, "12oz");
     }
 
     public void displayMenu() {
         System.out.println("== Food Menu ==");
-        for (int i = 0; i < menu.size(); i++) {
+        for (int i = 0; i < menu.length; i++) {
             System.out.print("[" + (i + 1) + "] ");
-            menu.get(i).displayInfo();
+            menu[i].displayInfo();
         }
     }
 
     public Food getFood(int index) {
-        return menu.get(index);
+        return menu[index];
     }
 
     public int getMenuSize() {
-        return menu.size();
+        return menu.length;
     }
 }
